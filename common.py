d00391d30cc7db368c749586293b081e97ac589b
Odin-son <song921216@gmail.com>
2023-04-12 17:53:29 +0900

    defined mvtec classes with project path

---
+++ common.py
@@ -0,0 +1,11 @@
+import os.path as osp
+
+CLASSES = {
+    0  :  'crack' ,
+    1  :  'faulty_imprint' ,
+    2  :  'good' ,
+    3  :  'poke' ,
+    4  :  'scratch' ,
+    5  :  'squeeze' }
+
+PROJ_DIR = osp.dirname(__file__)
